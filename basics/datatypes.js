6244645b4d474d060e122b1a02419019e7085ede
Khowsalya R <khowsalyar59@gmail.com>
2023-12-19 02:54:35 +0000

    add stack-heap-function

--- basics/datatypes.js
+++ basics/datatypes.js
@@ -7,20 +7,20 @@
 // BigInt
 // symbol mostly used in libraries
 
-let fullname ="kani" 
+let fullname ="kaniyamudhu" 
 let age = 6;
 let salary = null;
 let hike = undefined;
 let isLoggedIn = true;
 let largeNumber = 8888888888888888888888888n;
- const buttonId = symbol("Id")
+//  const buttonId = symbol("Id");
 
 
 //  refernce types -(HEAP)
 // object
 let userOject = {
     "name" : "Kani",
-    age : 20,
+    age : 21,
     isLoggedIn :true
 }
 
@@ -31,3 +31,10 @@ let naturalNumbers = [1,2,3,4,5,6,7,8,9,0]
 const sayhello = function(){
     console.log("logged in")
 };
+
+let anotherName = fullname;
+let anotherUser = userOject
+anotherName = "Kani Kutty"
+
+console.log(fullname,anotherName);
+console.log(userOject,anotherUser);
