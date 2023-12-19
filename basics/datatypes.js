96cc247a981546f8154dc2bc5eee65ddc8f019eb
Khowsalya R <khowsalyar59@gmail.com>
2023-12-19 02:22:21 +0000

    add datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,31 @@
+// primitive data types
+
+// String
+// Numbernull
+// undefined
+// boolean
+// BigInt
+// symbol mostly used in libraries
+
+let fullname ="kani" 
+let age = 6;
+let salary = null;
+let hike = undefined;
+let isLoggedIn = true;
+let largeNumber = 8888888888888888888888888n;
+ const buttonId = symbol("Id")
+
+
+//  refernce types
+// object
+let userOject = {
+    "name" : "Kani",
+    age : 20,
+    isLoggedIn :true
+}
+
+// Array
+let naturalNumbers = [1,2,3,4,5,6,7,8,9,0]
+
+// function
+const sayhello = function(){};
