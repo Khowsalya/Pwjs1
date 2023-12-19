d67a8056bb5cb127e845921d0e00d3336fde793d
Khowsalya R <khowsalyar59@gmail.com>
2023-12-19 01:34:14 +0000

    add variables type

---
+++ basics/variable.js
@@ -0,0 +1,11 @@
+// variables that we can change in future -let
+// Variables that we can't change in future -const
+
+let userEmail ="Gowsalya@gmail.com"
+const password ="12345"
+
+console.log(password);
+
+// Password is stord once in a memeory and it has become a special keyword so dnt wanna use it in double quotes
+
+// if we try to change password it will throw error.
