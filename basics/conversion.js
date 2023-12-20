0df3045a8a7be9616ebf6ac23bad59f8c4e5629c
Khowsalya R <khowsalyar59@gmail.com>
2023-12-20 01:11:40 +0000

    add type conversion

---
+++ basics/conversion.js
@@ -0,0 +1,41 @@
+let bankbalance = "qqq"
+
+let bankbalaceamount = Number(bankbalance)
+
+console.log(bankbalaceamount)
+
+console.log(typeof bankbalaceamount)
+
+console.log(NaN == NaN)
+
+console.log(NaN === NaN)
+
+
+let firstname = "khowsalya"
+
+let lastname = "Ramanathan"
+
+let evennumber = 6
+
+let oddnumber = "7"
+
+// odd number type --number
+console.log(`My serial number ${evennumber} ${typeof +oddnumber}`)
+
+// oddnumber type is string here
+
+console.log(`My serial number ${evennumber} ${typeof oddnumber}`)
+
+
+
+console.log( 5 + NaN)
+
+console.log(5 + null)
+
+console.log(5 +undefined)
+
+console.log("ajjj"+undefined)
+
+console.log("jhh"+null)
+
+console.log(`My fullname is ${firstname+ +lastname}` )
